38e0408635215433fc6084346c5a16062ead97b4
sptkl <caobaiyue@gmail.com>
2019-09-15 09:21:09 -0400

    2:1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
 import React from 'react'
 import Note from './components/Note'
 
-const App = ({ notes }) => {
-  const rows = () => notes.map(note =>
-    <Note
-      key={note.id}
-      note={note}
-    />
-  )
+const App = ({ notes }) => { 
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+  const Header = ({name}) =>{
+    return (
+        <div>
+            <h1> {name} </h1>
+        </div>
+    )
+  }
+  const Course = ({course}) => {
+    return (
+      <div>
+        <Header name={course.name} />
+        <ul>
+          {course.parts.map(part => <li key={part.id}>{part.name} {part.exercises}</li>)}
+        </ul>
+      </div>
+    )
+  }
 
   return (
     <div>
-      <h1>Notes</h1>
-      <ul>
-        {rows()}
-      </ul>
+      <Course course={course} />
     </div>
   )
 }
